16e32e1be75fcd457cfb370f24935d2db5fbc9cd
zhanghang <286910682@qq.com>
2019-07-12 18:03:27 +0800

    添加公共模块依赖并扫描进入容器

--- fm-user/src/main/java/com/zh/fmuser/FmUserApplication.java
+++ fm-user/src/main/java/com/zh/fmuser/FmUserApplication.java
@@ -4,7 +4,9 @@ package com.zh.fmuser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.zh")
 @EnableEurekaClient
 @SpringBootApplication
 public class FmUserApplication {
--- fm-user/src/main/java/com/zh/fmuser/controller/HelloController.java
+++ fm-user/src/main/java/com/zh/fmuser/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.zh.fmuser.controller;
 
+import com.zh.fmcommon.enums.AppResultCodeEnum;
+import com.zh.fmcommon.exception.BusinessException;
+import com.zh.fmcommon.pojo.dto.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "hello fm-user";
+    public Result hello(String sequenceId){
+        log.info("hello fm-user:{}",sequenceId);
+        throw new BusinessException(AppResultCodeEnum.FAIL);
     }
 }
