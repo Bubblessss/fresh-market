468c94c6ce611856e1cc6456545f043cf03e103b
zhanghang <286910682@qq.com>
2019-07-12 18:03:42 +0800

    添加公共模块依赖并扫描进入容器

--- fm-order/src/main/java/com/zh/fmorder/FmOrderApplication.java
+++ fm-order/src/main/java/com/zh/fmorder/FmOrderApplication.java
@@ -3,7 +3,9 @@ package com.zh.fmorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.zh")
 @EnableEurekaClient
 @SpringBootApplication
 public class FmOrderApplication {
--- fm-order/src/main/java/com/zh/fmorder/controller/HelloController.java
+++ fm-order/src/main/java/com/zh/fmorder/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.zh.fmorder.controller;
 
+import com.zh.fmcommon.enums.AppResultCodeEnum;
+import com.zh.fmcommon.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello fm-order";
+        log.info("hello fm-order");
+        throw new BusinessException(AppResultCodeEnum.USER_NOT_EXIST);
     }
 }
