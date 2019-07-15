546639ee4c7591b350676fc02e93acda0385bd79
zhanghang <zhanghang9202@163.com>
2019-07-15 17:50:49 +0800

    添加日志类型

--- fm-common/src/main/java/com/zh/fmcommon/constance/MongoDBConstance.java
+++ fm-common/src/main/java/com/zh/fmcommon/constance/MongoDBConstance.java
@@ -7,4 +7,8 @@ package com.zh.fmcommon.constance;
 public class MongoDBConstance {
 
     public static final String COLLECTION_NAME_PRE_APP_VISIT_LOG = "app_visit_log_";
+
+    public static final Integer LOG_TYPE_REQUEST = 0;
+    
+    public static final Integer LOG_TYPE_RESPONSE = 1;
 }
